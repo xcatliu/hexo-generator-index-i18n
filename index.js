2fbcf8f2c890d9fdd7667f38aafdcf532e946ca0
xcatliu <xcatliu@gmail.com>
2016-06-24 15:16:50 +0800

    Remove object-assign

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 /* global hexo */
-const assign = require('object-assign');
 const pagination = require('hexo-pagination');
 
-hexo.config.index_generator = assign({
+hexo.config.index_generator = Object.assign({
   per_page: typeof hexo.config.per_page === 'undefined' ? 10 : hexo.config.per_page,
   order_by: '-date',
 }, hexo.config.index_generator);
