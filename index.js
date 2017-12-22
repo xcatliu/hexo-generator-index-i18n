1185c08f6c1b420e0137ee5bdba1d2c6ceba651f
Jose V Herrera <josevictorherrera@gmail.com>
2017-12-22 16:26:49 +0000

    fix path config not being used

--- index.js
+++ index.js
@@ -33,15 +33,16 @@ hexo.config.index_generator = Object.assign({
 hexo.extend.generator.register('index-i18n', function indexI18nGenerator(locals) {
   const config = this.config;
   const posts = locals.posts.sort(config.index_generator.order_by);
+  const path = config.index_generator.path || '';
   const languages = [].concat(config.language || [])
     .filter(lang => lang !== 'default');
   const defaultLanguage = languages[0];
   let indexPages = [].concat.apply([],
-    languages.map(lang => getIndexPages(lang, lang, posts, config))
+    languages.map(lang => getIndexPages(lang + '/' + path, lang, posts, config))
   );
 
   if (config.index_generator.single_language_index && defaultLanguage) {
-    indexPages = indexPages.concat(getIndexPages('', defaultLanguage, posts, config));
+    indexPages = indexPages.concat(getIndexPages(path, defaultLanguage, posts, config));
   }
 
   return indexPages;
